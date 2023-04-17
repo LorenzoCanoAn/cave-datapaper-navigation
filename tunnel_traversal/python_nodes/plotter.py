d3a59c3fb1f63782537ac81b3fb4c86554d1bc80
lcano@unizar.es <lcano@unizar.es>
2023-04-17 10:55:28 +0200

    -

---
+++ tunnel_traversal/python_nodes/plotter.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+import matplotlib.pyplot as plt
+import rospy
+from std_msgs.msg import Float32MultiArray
+import threading
+import numpy as np
+
+
+class PlotterNode:
+    def __init__(self):
+        self._updated_vector = False
+        rospy.init_node(name="~plotter_node")
+        self._plot_vector = rospy.get_param("~plot_vector", default=False)
+        if self._plot_vector:
+            rospy.Subscriber(
+                name="internals/vector",
+                data_class=Float32MultiArray,
+                callback=self.vector_callback,
+            )
+
+    def vector_callback(self, msg: Float32MultiArray):
+        self._vector_data_y = msg.data
+        self._vector_data_x = np.arange(len(self._vector_data_y), step=1)
+        self._updated_vector = True
+
+    def draw_loop(self):
+        self._fig = plt.figure(figsize=(5, 5))
+        self._ax1 = self._fig.add_subplot(111)
+        # Actors
+        (self._vector_artist,) = self._ax1.plot([], lw=6, c="b")
+        # Set apparence
+        self._ax1.set_xlim(0, 360)
+        self._ax1.set_ylim(0, 1)
+        # Draw
+        self._fig.canvas.draw()
+        self._ax1background = self._fig.canvas.copy_from_bbox(self._ax1.bbox)
+        plt.show(block=False)
+        while not rospy.is_shutdown():
+            if self._updated_vector:
+                self._vector_artist.set_data(self._vector_data_x, self._vector_data_y)
+            # Restore background
+            self._fig.canvas.restore_region(self._ax1background)
+            # Redraw artists
+            self._ax1.draw_artist(self._vector_artist)
+            # Fill axis rectangle
+            self._fig.canvas.blit(self._ax1.bbox)
+            self._fig.canvas.flush_events()
+
+
+def main():
+    node = PlotterNode()
+    rospy_thread = threading.Thread(target=rospy.spin)
+    rospy_thread.start()
+    node.draw_loop()
+    rospy_thread.join()
+
+
+if __name__ == "__main__":
+    main()
--- tunnel_traversal/python_nodes/tunnel_traversal_navigation.py
+++ tunnel_traversal/python_nodes/tunnel_traversal_navigation.py
@@ -1,12 +1,37 @@
 #!/bin/python3
 import rospy
 from sensor_msgs.msg import Image
-from sensor_msgs.point_cloud2 import read_points
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool, Float32MultiArray, MultiArrayDimension
+from std_msgs.msg import Bool, Float32MultiArray, MultiArrayDimension, Float32
 import numpy as np
-from scipy.ndimage import gaussian_filter
 import cv2
+
+
+def angle_rad_to_index(angle):
+    angle_deg = np.rad2deg(angle)
+    return angle_deg_to_index(angle)
+
+
+def angle_deg_to_index(angle):
+    angle = int(angle)
+    index = (-angle + 180) % 360
+    return index
+
+
+def index_to_angle_deg(index):
+    angle = (-index + 360) % 360
+    angle = (angle - 180) % 360
+    return angle
+
+
+def index_to_angle_rad(index):
+    return np.deg2rad(index_to_angle_deg(index))
+
+
+def warp_rad_angle_pi(angle):
+    if angle > np.pi:
+        return angle - 2 * np.pi
+    return angle
 
 
 class TunnelTraversalNavigator:
@@ -14,7 +39,10 @@ class TunnelTraversalNavigator:
         rospy.init_node("tunnel_traversal_navigator")
         toggle_topic = rospy.get_param("~toggle_topic")
         image_topic = rospy.get_param("~image_topic")
-        velocity_topic = rospy.get_param("~velocity_topic")
+        angle_topic = rospy.get_param("~angle_topic")
+        self._frontal_angles_range = rospy.get_param(
+            "~frontal_angles_range_deg", (-40, 40)
+        )
         self._publish_vector = rospy.get_param("~config/publish_vector", default=True)
         self.toggle_subscriber = rospy.Subscriber(
             toggle_topic, Bool, callback=self.toggle_callback
@@ -22,29 +50,52 @@ class TunnelTraversalNavigator:
         self.ptcl_subscriber = rospy.Subscriber(
             image_topic, Image, callback=self.image_callback
         )
-        self.vel_pub = rospy.Publisher(velocity_topic, Twist, queue_size=2)
+        self.angle_pub = rospy.Publisher(angle_topic, Float32, queue_size=2)
         if self._publish_vector:
             self._vector_publisher = rospy.Publisher(
-                "~internals/vector", Float32MultiArray, queue_size=2
+                "internals/vector", Float32MultiArray, queue_size=2
             )
-        self._publish_vel = False
+        self._publish_vel = True
 
     def image_callback(self, ptcl_msg: Image):
-        if not self._publish_vel:
-            return
         dtype = np.dtype(np.float32)
         channels = 1
         dtype = dtype.newbyteorder(">" if ptcl_msg.is_bigendian else "<")
         shape = (ptcl_msg.height, ptcl_msg.width, channels)
         img = np.frombuffer(ptcl_msg.data, dtype=dtype).reshape(shape)
         img.strides = (ptcl_msg.step, dtype.itemsize * channels, dtype.itemsize)
-        conv_img = cv2.GaussianBlur(img, (15, 15), cv2.BORDER_WRAP)
+        img = np.array(img)
+        # img[np.where(img == 0.0)] = 1
+        conv_img = cv2.GaussianBlur(img, (15, 31), cv2.BORDER_WRAP)
         conv_vector = np.reshape((conv_img[7, :] + conv_img[8, :]) / 2, -1)
-        print(conv_vector)
+        print(conv_vector.shape)
+        print(img.shape)
         if self._publish_vector:
             vector_msg = Float32MultiArray()
             vector_msg.data = conv_vector
             self._vector_publisher.publish(vector_msg)
+
+        idxi = angle_deg_to_index(self._frontal_angles_range[0])
+        idxj = angle_deg_to_index(self._frontal_angles_range[1])
+        idxk = idxj + np.argmax(conv_vector[idxj:idxi])
+        angle_rad_to_advance = warp_rad_angle_pi(index_to_angle_rad(idxk))
+        # To correct the angle so that the robot is centered
+        vector_at_medium_height = img[7, :]
+        distances_at_the_right = vector_at_medium_height[
+            angle_deg_to_index(-80) : angle_deg_to_index(-100)
+        ]
+        distances_at_the_left = vector_at_medium_height[
+            angle_deg_to_index(100) : angle_deg_to_index(80)
+        ]
+        avg_dist_right = np.average(distances_at_the_right)
+        avg_dist_left = np.average(distances_at_the_left)
+        print(f"right{avg_dist_right} left: {avg_dist_left}")
+        if avg_dist_left > avg_dist_right:
+            angle_rad_to_advance += np.deg2rad(2)
+        else:
+            angle_rad_to_advance -= np.deg2rad(2)
+        if self._publish_vel:
+            self.angle_pub.publish(Float32(angle_rad_to_advance))
 
     def toggle_callback(self, msg: Bool):
         self._publish_vel = msg.data
